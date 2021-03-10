82e778b38c5cc9d8310681596e60ca66d69442d9
Harry Cowshed <Harry@wearecowshed.co.uk>
2021-03-10 12:51:44 +0000

    loadbootstrap function added

--- functions.php
+++ functions.php
@@ -1,4 +1,16 @@
 <?php
+// Enqueue scripts and styles
+function loadBootstrap() {
+    // CSS
+    wp_register_style('bootCSS', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css', array(), false, 'all');
+    wp_enqueue_style('bootCSS');
+
+    // JS
+    wp_enqueue_script('jquery'); // should be already known
+    wp_register_script('bootJS', get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js', 'jquery', false, true);
+    wp_enqueue_script('bootJS');
+}
+add_action( 'wp_enqueue_scripts', 'loadBootstrap' );
 
 // Link CSS 
 function loadCss()
@@ -8,7 +20,7 @@ function loadCss()
 }
 add_action('wp_enqueue_scripts', 'loadCss');
 
-// Our custom post type function
+// Custom post type function
 function create_posttype() {
     $loop = array(
         'labels' => [
